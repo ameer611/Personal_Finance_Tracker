07342cc46f096351b9c3346db52e6b313b5c0619
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-07-13 11:38:01 +0500

    changed User model

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -134,3 +134,5 @@ STATIC_ROOT = BASE_DIR / '/staticfiles/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user_app.User'
