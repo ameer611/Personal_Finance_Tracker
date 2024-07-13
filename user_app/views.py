19c71a097b4172a5315b089cfa03e08bb00df48a
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-07-13 20:18:24 +0500

    corrected some bugs on views.py file

--- user_app/views.py
+++ user_app/views.py
@@ -11,8 +11,9 @@ from user_app.serializer import UserSerializer
 
 class RegisterView(APIView):
     def post(self, request):
-        serializer = UserSerializer
+        serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
 
 class LoginView(APIView):
@@ -33,7 +34,7 @@ class LoginView(APIView):
             'iat': datetime.datetime.utcnow(),
         }
 
-        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
 
         response = Response()
         response.set_cookie(key='jwt', value=token, httponly=True)
@@ -41,3 +42,27 @@ class LoginView(APIView):
 
         return response
 
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            'message': 'success'
+        }
+        return response
