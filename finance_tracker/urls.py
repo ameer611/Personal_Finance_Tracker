6856725e4a6f4fc0380e593e96813049f292b68a
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-07-13 11:42:01 +0500

    added urls.py file of user_app

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('finance_app.urls')),
+    path('user/', include('user_app.urls')),
 ]
