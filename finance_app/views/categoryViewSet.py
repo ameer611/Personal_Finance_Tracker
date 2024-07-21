25fc4ef594902b97891137011e8e06e5e2a0dcf2
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-07-21 13:55:31 +0500

    add validation to category serializer and transaction serializer updated

--- finance_app/views/categoryViewSet.py
+++ finance_app/views/categoryViewSet.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from finance_app.models import Category
@@ -6,7 +7,11 @@ from finance_app.serializers import CategorySerializer
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     def get_queryset(self):
-        queryset = Category.objects.all().order_by('name')
+        queryset = Category.objects.filter(user=self.request.user).order_by('name')
         return queryset
--- finance_app/views/transactionViewSet.py
+++ finance_app/views/transactionViewSet.py
@@ -1,4 +1,6 @@
+from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
+
 
 from finance_app.models import Transaction
 from finance_app.serializers import TransactionSerializer
@@ -6,7 +8,11 @@ from finance_app.serializers import TransactionSerializer
 
 class TransactionViewSet(ModelViewSet):
     serializer_class = TransactionSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
-        queryset = Transaction.objects.all()
+        queryset = Transaction.objects.filter(user=self.request.user)
         return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
