a0ac39d8a35e3bc723406c90b175905c18563a7d
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-07-21 14:00:05 +0500

    added name_validation function and updated category validation function

--- finance_app/serializers/categorySerializer.py
+++ finance_app/serializers/categorySerializer.py
@@ -4,7 +4,18 @@ from finance_app.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Category
-        fields = ('id', 'name')
+        fields = ('id', 'name', 'user')
+        read_only_fields = ['user']
+
+
+    def validate_name(self, value):
+        user = self.context['request'].user
+        if Category.objects.filter(user=user, name=value).exists():
+            raise serializers.ValidationError("Such category already exists!")
+        return value
+
+
         
--- finance_app/serializers/transactionSerializer.py
+++ finance_app/serializers/transactionSerializer.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+import datetime
 
 from rest_framework import serializers
 
-from finance_app.models import Transaction
+from finance_app.models import Transaction, Category
 from finance_app.serializers import CategorySerializer
 
 
@@ -10,7 +10,7 @@ class TransactionSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     class Meta:
         model = Transaction
-        fields = [
+        fields = (
             'id',
             'amount',
             'description',
@@ -18,8 +18,10 @@ class TransactionSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'category',
-            'transaction_type'
-        ]
+            'transaction_type',
+            'user'
+        )
+        read_only_fields = ['user']
 
     def validate_amount(self, value):
         if value <= 0:
@@ -27,12 +29,18 @@ class TransactionSerializer(serializers.ModelSerializer):
         return value
 
     def validate_date(self, value):
-        if value < datetime(2015, 1, 1):
+        if value < datetime.date(2015, 1, 1):
             raise serializers.ValidationError('Date must be after 2015-01-01')
         return value
 
     def validate_transaction_type(self, value):
         if value not in ['expense', 'income']:
             raise serializers.ValidationError('Transaction type must be either expense or income')
+        return value
+
+    def validate_category(self, value):
+        user = self.context['request'].user
+        if not Category.objects.filter(user=user, id=value.id).exists():
+            raise serializers.ValidationError("Category does not exist")
         return value
 
