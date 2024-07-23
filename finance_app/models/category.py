ea61e8fa36adfe2f7253f2a377d7ee700cdb02f3
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-07-23 18:28:27 +0500

    added user to __str__ method

--- finance_app/models/category.py
+++ finance_app/models/category.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.db import models
 
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.user}"
