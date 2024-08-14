313b6e867870cf102583c1631d2795df48fea978
Mukhammad Usmonov <oo1amirmuhammad@gmail.com>
2024-08-14 19:19:38 +0500

    changed swagger url

--- finance_app/urls.py
+++ finance_app/urls.py
@@ -1,23 +1,9 @@
 from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
-from drf_yasg import openapi
 
 from finance_app.views import TransactionViewSet, CategoryViewSet
 from finance_app.views.statisticsViewSet import get_user_monthly_income_expense_data, \
     get_user_monthly_category_income_data, get_user_monthly_category_expense_data
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Finance Tracker Transactions",
-        default_version='v1',
-        description="API for track your incomes and outcomes",
-        contact=openapi.Contact(email="oo1amirmuhammad@gmail.com"),
-    ),
-    public=True,
-    permission_classes=(AllowAny,),
-)
 
 
 
@@ -35,7 +21,4 @@ urlpatterns = [
     path('statistics/', get_user_monthly_income_expense_data, name='monthly-overall-statistics'),
     path('statistics/income/', get_user_monthly_category_income_data, name='monthly-income-statistics'),
     path('statistics/expense/', get_user_monthly_category_expense_data, name='monthly-expense-statistics'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-
 ]
--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -2,9 +2,24 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Finance Tracker",
+        default_version='v1',
+        description="API for track your incomes and outcomes",
+        contact=openapi.Contact(email="oo1amirmuhammad@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('finance_app.urls')),
     path('user/', include('user_app.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
